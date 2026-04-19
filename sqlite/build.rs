5d6507687e6910bb773b6968dbd9910aba4dafdc
coredump <rcoredump+github@proton.me>
2026-04-19 16:17:13 +0200

    wip

--- sqlite/build.rs
+++ sqlite/build.rs
@@ -1,38 +1,58 @@
 use std::process::Command;
 
-const BUILD_DIR: &str = "sqlite3_build";
-
-const SQLITE_DIR: &str = "sqlite_c_src";
-const CONFIGURE_PATH: &str = "../sqlite_c_src/configure";
-
-fn main() {
-    println!("cargo:rerun-if-changed={}", SQLITE_DIR);
-    println!("cargo:rerun-if-changed={}", "build.rs");
-    println!("cargo:rerun-if-changed={}", "libsqlite3.a");
-
-    build_sqlite_static_lib();
-    link_libsqlite3();
+struct SQLiteBuilder {
+    build_dir: std::path::PathBuf,
+    configure_path: std::path::PathBuf,
 }
 
-fn create_sqlite_build_dir() {
-    if std::fs::exists(BUILD_DIR).unwrap() {
-        return;
+impl SQLiteBuilder {
+    const BUILD_DIR: &'static str = "sqlite3_build";
+
+    const SOURCE_DIR: &'static str = "sqlite_c_src";
+
+    fn new() -> Self {
+        let current_dir = std::env::current_dir().expect("unable to get current directory");
+        let build_dir = current_dir.join(Self::BUILD_DIR);
+        let script_path = current_dir.join("build.rs");
+        let configure_path = current_dir.join(Self::SOURCE_DIR).join("configure");
+        let sqlite_3_h_path = current_dir.join(Self::SOURCE_DIR).join("sqlite3.h");
+        let sqlite_3_c_path = current_dir.join(Self::SOURCE_DIR).join("sqlite3.c");
+
+        println!("cargo:rerun-if-changed={}", script_path.display());
+        println!("cargo:rerun-if-changed={}", configure_path.display());
+        println!("cargo:rerun-if-changed={}", sqlite_3_h_path.display());
+        println!("cargo:rerun-if-changed={}", sqlite_3_c_path.display());
+
+        Self {
+            build_dir,
+            configure_path,
+        }
     }
 
-    std::fs::create_dir(BUILD_DIR).expect("unable to create sqlite build dir");
+    fn build(&self) {
+        self.create_build_dir();
+        self.configure_build();
+        Command::new(&self.configure_path).current_dir(&self.build_dir).output().expect("failed to run configure");
+    }
+
+    fn create_build_dir(&self) {
+        if !self.build_dir.exists() {
+            std::fs::create_dir(&self.build_dir).expect("failed to create build dir");
+        }
+    }
+
+    fn configure_build(&self) {
+        Command::new(&self.configure_path).current_dir(&self.build_dir).output().expect("failed to run configure");
+    }
+
+    fn link(&self) {
+        println!("cargo:rustc-link-search=native={}", self.build_dir.display());
+        println!("cargo:rustc-link-lib=static=sqlite3");
+    }
 }
 
-fn configure_sqlite_build() {
-    create_sqlite_build_dir();
-    Command::new(CONFIGURE_PATH).current_dir(BUILD_DIR).output().expect("unable to configure sqlite");
-}
-
-fn build_sqlite_static_lib() {
-    configure_sqlite_build();
-    Command::new("make").arg("libsqlite3.a").current_dir(BUILD_DIR).output().expect("unable to build libsqlite3.a");
-}
-
-fn link_libsqlite3() {
-    println!("cargo:rustc-link-search=native={}", BUILD_DIR);
-    println!("cargo:rustc-link-lib=sqlite3");
+fn main() {
+    let sqlite3_builder = SQLiteBuilder::new();
+    sqlite3_builder.build();
+    sqlite3_builder.link();
 }
